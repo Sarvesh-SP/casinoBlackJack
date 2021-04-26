93fef0f3b31645d069507adfaf1f4392de2ab656
Sarvesh-SP <sarvesh.sp2013@gmail.com>
2021-04-26 08:45:00 +0530

    Added dealing and shuffling

--- casino.py
+++ casino.py
@@ -1,3 +1,6 @@
+import random
+
+
 class Card():
     """Simulate a single card with rank, value and suit."""
 
@@ -15,10 +18,26 @@ class Deck():
     """Simulate a deck of 52 indivitual playing cards."""
 
     def __init__(self):
+        """A list to hold all future cards in the deck"""
         self.cards = []
 
     def build_deck(self):
         """Build a deck constituting of 52 unique cards."""
         suits = ['Hearts', 'Diamonds', 'Spades', 'Clubs']
         ranks = {'2': 2, '3': 3, '4': 4, '5': 5, '6': 6, '7': 7,
-                 '8': 8, '9': 9, '10': 10, 'J': 10, 'Q': 10, 'K': 10, 'A': 11}
+                 '8': 8, '9': 9, '10': 10, 'J': 10, 'Q': 10, 'K': 10, 'A': 11, }
+
+        for suit in suits:
+            for rank, value in ranks.items():
+                card = Card(rank, value, suit)
+                self.cards.append(card)
+
+      def shuffle_deck(self):
+        """Shuffle a deck of cards"""
+        random.shuffle(self.cards)
+
+      def deal_card(self):
+        """Remove a card from the deck to be dealt"""
+        
+        card = self.cards.pop()
+        return card
